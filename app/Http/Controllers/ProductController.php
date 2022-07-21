d673d7640c18fddcde1c4374b4de75174837c999
boygunawan <erdyansyahboy@gmail.com>
2022-07-21 15:15:34 +0700

    add function store data

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -27,4 +27,29 @@ class ProductController extends Controller
         return view('admin.product.form');
 
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'nama_produk' => 'required',
+            'harga' => 'required',
+            'kategori' => 'required',
+        ],
+        [
+            'nama_produk.required' => 'Nama Produk harus diisi!!',
+            'kategori.required' => 'Kategori harus diisi!!',
+            'harga.required' => 'Harga harus diisi!!',
+        ]);
+
+        $data = array(
+             'nama_produk' => $request->nama_produk,
+             'kategori' => $request->kategori,
+             'harga' => $request->harga,
+             'status' => 'bisa dijual',
+        );
+
+        Product::insert($data);
+
+        return redirect('/product')->with('status', 'Produk Anda Sudah Ditambahkan'); 
+    }
 }
