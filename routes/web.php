bbe1b8f06859a1116bb96efd920fde04d241098c
boygunawan <erdyansyahboy@gmail.com>
2022-07-21 15:12:59 +0700

    add route display data product & form create product

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/product', 'ProductController@index');
+Route::get('/product/create', 'ProductController@create');
